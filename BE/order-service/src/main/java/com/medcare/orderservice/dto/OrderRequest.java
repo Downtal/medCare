456bc620ac8e7e3e93d80acf668a1005e54f8abc
minhcuong04 <22130032@st.hcmuaf.edu.vn>
2026-04-28 16:14:25 +0700

    fix compilation error: add missing BigDecimal import in OrderRequest

--- BE/order-service/src/main/java/com/medcare/orderservice/dto/OrderRequest.java
+++ BE/order-service/src/main/java/com/medcare/orderservice/dto/OrderRequest.java
@@ -3,9 +3,15 @@ package com.medcare.orderservice.dto;
 import com.medcare.orderservice.entity.PaymentMethod;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import java.math.BigDecimal;
 
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @com.fasterxml.jackson.annotation.JsonIgnoreProperties(ignoreUnknown = true)
 public class OrderRequest {
     @NotBlank(message = "Tên người nhận không được để trống")
