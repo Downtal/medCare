387fb3e2191cfceecc215a2ef9dc952a489800db
minhcuong04 <22130032@st.hcmuaf.edu.vn>
2026-04-28 16:15:12 +0700

    fix compilation error: restore missing lombok.Data import in OrderRequest

--- BE/order-service/src/main/java/com/medcare/orderservice/dto/OrderRequest.java
+++ BE/order-service/src/main/java/com/medcare/orderservice/dto/OrderRequest.java
@@ -5,6 +5,7 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 import java.math.BigDecimal;
 
